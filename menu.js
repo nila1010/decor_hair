21d9eb294c5fc0ff2dc8d87406547858e7ef6510
NL <nila0001@stud.kea.dk>
2023-05-26 11:53:37 +0200

    fade nav

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
-const nav = document.getElementsByTagName("header");
+const nav = document.querySelector("header");
+
 window.onscroll = function () {
-  if (document.body.scrollTop >= 200) {
-    header.classList.add("header");
-    header.classList.remove("header_transparent");
-  } else {
-    header.classList.add("header_transparent");
-    header.classList.remove("header");
-  }
+  scrollFunction();
 };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    nav.classList.add("header_transparent");
+    nav.classList.remove("header");
+  } else {
+    nav.classList.add("header");
+    nav.classList.remove("header_transparent");
+  }
+}
